651ef366a1a55987390da9f7b5c8012679217675
bogomilmihaylov <bogomilmihaylov@live.com>
2019-05-30 18:31:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -34,7 +34,7 @@ def now_serving(current_line)
     current_line.shift()
     puts("Currently serving ", served_customer)
   end
-  served_customer
+  served_customer = katz_deli
   return current_line
 end
 
